db9c012c7002cd951f6bb18288a19f5ef282ecec
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-03 14:38:29 -0400

    add date heading and unix timestamp to the end of each tests data

--- capture-speed.sh
+++ capture-speed.sh
@@ -17,7 +17,22 @@ if [ -f "$OUTPUT_FILE" ];
 then
 	speedtest --progress=no --format=csv >> "$OUTPUT_FILE"
 else
-	speedtest --progress=no --output-header --format=csv >> "$OUTPUT_FILE"
+	speedtest --progress=no --output-header --format=csv >> "$OUTPUT_FILE.tmp"
+	# add date header to end of first line and set that new content as the file content
+	#https://stackoverflow.com/questions/26958327/bash-script-append-text-to-first-line-of-a-file#26958397
+	sed '1 s/$/,date/' "$OUTPUT_FILE.tmp" > "$OUTPUT_FILE"
+
+	rm "$OUTPUT_FILE.tmp"
+
 fi
 
+# remove the last newline
+# https://superuser.com/questions/1332947/append-to-end-to-line-without-newline
+truncate -s-1 "$OUTPUT_FILE"
+
+
+# add the current timestamp	to the end
+# https://timestamp.online/article/how-to-get-current-timestamp-in-bash
+
+echo ",$(date +"%s")" >> "$OUTPUT_FILE"
 
